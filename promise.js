689df3d3ac9d62b5f509667f93013c7e0a4b1315
Imam Abu Hanifah <imam.abuhanifah@ai.astra.co.id>
2022-09-18 23:42:08 +0700

    Adjustment promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,24 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (value) => {
+  try {
+    const getData1 = await promiseTheaterVGC();
+    
+    getData1.push.apply(getData1, await promiseTheaterIXX());
+
+    const getDataAll = getData1.map((data) => {
+      return data.hasil;
+    })
+    
+    const result = "Jumlah penonton yang " + value + " = " + getDataAll.filter(data => data === value).length;
+    
+    console.log(result);
+
+  } catch (error) {
+    alert(error);
+  }
+};
 
 module.exports = {
   promiseOutput,
